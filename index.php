2bc02ad4a3452b571266953562f5ed11bc2b49b0
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-09 17:50:41 +0200

    added methos for good grades

--- index.php
+++ index.php
@@ -7,17 +7,26 @@
         public $year;
         public $imgUrl;
 
-        function __construct($_name, $_vote, $_filmDirector, $_year, $_imgUrl, ){
+        function __construct($_name, $_filmDirector, $_vote, $_year, $_imgUrl, ){
             $this->name = $_name;
             $this->vote = $_vote;
             $this->filmDirector = $_filmDirector;
             $this->year = $_year;
             $this->imgUrl = $_imgUrl;
         }
+
+        function isGoodGrade($num){
+            if ($num < $this->vote){
+                return "il film ha voto sufficiente!";
+            } else {
+                return "il film ha voto insufficiente!";
+            }
+        }
     }
-    $firstFilm = new Movie("Inception", "Christopher Nolan", 4.65, "2010", "https://images.everyeye.it/img-notiziealt/inception-warner-bros-svela-data-atteso-ritorno-cinema-italiani-v3-462812-350x16.jpg");
-    $secondFilm = new Movie("Tenet", "Christopher Nolan", 3.45, "2020", "https://www.labottegadihamlin.it/wp-content/uploads/2020/08/tenet-locandina-scaled-870x500-1598694906.jpg");
+    $firstFilm = new Movie("Inception", "Christopher Nolan", 5, "2010", "https://images.everyeye.it/img-notiziealt/inception-warner-bros-svela-data-atteso-ritorno-cinema-italiani-v3-462812-350x16.jpg");
+    $secondFilm = new Movie("Tenet", "Christopher Nolan", 2, "2020", "https://www.labottegadihamlin.it/wp-content/uploads/2020/08/tenet-locandina-scaled-870x500-1598694906.jpg");
     
+
 ?>
 
 <!DOCTYPE html>
@@ -34,28 +43,34 @@
     <main class="container">
         <h1 class="fw-bold text-center p-4">PHP OOP MOVIE</h1>
         <div class="d-flex justify-content-center gap-4">
-            <div class="card" style="width: 18rem;">
-                <img src="<?= $firstFilm->imgUrl ?>" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="text-center fw-bold">
-                        Titolo: <?= $firstFilm->name?>
-                    </h5>
-                    <p class="card-text text-center mb-0">Regista: <?= $firstFilm->filmDirector ?></p>
-                    <p class="card-text text-center mb-0">Voto: <?= $firstFilm->vote ?></p>
-                    <p class="card-text text-center">Anno: <?= $firstFilm->year ?></p>
+            <div>
+                <div class="card" style="width: 18rem;">
+                    <img src="<?= $firstFilm->imgUrl ?>" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="text-center fw-bold">
+                            Titolo: <?= $firstFilm->name?>
+                        </h5>
+                        <p class="card-text text-center mb-0">Regista: <?= $firstFilm->filmDirector ?></p>
+                        <p class="card-text text-center mb-0">Voto: <?= $firstFilm->vote ?> stelle</p>
+                        <p class="card-text text-center">Anno: <?= $firstFilm->year ?></p>
+                    </div>
                 </div>
+                <p><?= $firstFilm->isGoodGrade(4) ?></p>
             </div>
-            <div class="card" style="width: 18rem;">
-                <img src="<?= $secondFilm->imgUrl ?>" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="text-center fw-bold">
-                        Titolo: <?= $secondFilm->name ?>
-                    </h5>
-                    <p class="card-text text-center mb-0">Regista: <?= $secondFilm->filmDirector ?></p>
-                    <p class="card-text text-center mb-0">Voto: <?= $secondFilm->vote ?></p>
-                    <p class="card-text text-center">Anno: <?= $secondFilm->year ?></p>
+            <div>
+                <div class="card" style="width: 18rem;">
+                    <img src="<?= $secondFilm->imgUrl ?>" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="text-center fw-bold">
+                            Titolo: <?= $secondFilm->name ?>
+                        </h5>
+                        <p class="card-text text-center mb-0">Regista: <?= $secondFilm->filmDirector ?></p>
+                        <p class="card-text text-center mb-0">Voto: <?= $secondFilm->vote ?> stelle</p>
+                        <p class="card-text text-center">Anno: <?= $secondFilm->year ?></p>
+                    </div>
                 </div>
-            </div>
+                <p><?= $firstFilm->isGoodGrade(4) ?></p>
+            </div>   
         </div>    
     </main>
 </body>
