705e0487605735ceaf8bab42f29a1409b656ea4e
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-09 16:57:37 +0200

    printed on page a card with a movie with his istance variable

--- index.php
+++ index.php
@@ -19,6 +19,21 @@ Possiamo ad esempio organizzare il codice
 -mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare classes/
 organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati. 
 -->
+
+<?php 
+
+    class Movie {
+        public $name;
+        public $vote;
+        public $filmDirector;
+        public $year;
+        public $imgUrl;
+
+    }
+    $firstFilm = new Movie();
+    $url = $firstFilm->imgUrl = "https://pad.mymovies.it/filmclub/2009/03/027/immpg25.jpg" 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,8 +42,22 @@ organizzando il layout dividendo la struttura ed i contenuti in file e parziali 
     <title>php-oop-movie</title>
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
 </head>
 <body>
-    
+    <main class="container">
+
+        <div class="card" style="width: 18rem;">
+            <img src="<?= $url ?>" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="text-center fw-bold">
+                    <?= $firstFilm->name = "Inception"?>
+                </h5>
+                <p class="card-text text-center">Regista: <?= $firstFilm->filmDirector = "Christopher Nolan" ?></p>
+                <p class="card-text text-center">Voto: <?= $firstFilm->vote = "9.8" ?></p>
+                <p class="card-text text-center">Anno: <?= $firstFilm->year = "2010" ?></p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
